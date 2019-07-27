522fa0507639e31ed97c05a8cd6d3327e44bd5b1
Nwankwo Samuel <nwankwosami@gmail.com>
2019-07-27 01:06:10 +0100

    learn how to store and track information with variables

---
+++ JavaScript-basics/main.js
@@ -0,0 +1,32 @@
+var test = "homecoming queen ";
+console.log(test.length);
+alert(test.length);
+document.write(test.length);
+
+//capturing user input
+var vistorName = prompt("what is your name?");
+alert(vistorName);
+var message = "Hello " + vistorName;
+message = message + " we are so glad that you came back to visit ";
+message = message + "Thank you ";
+message += " this requires less typing";
+document.write(message);
+document.write(message.length);
+
+console.log(test.toLowerCase());
+console.log(test.toUpperCase());
+
+
+//creating a shoutout app
+var stringToShout = prompt("what should I shout?");
+var shout = stringToShout.toUpperCase();
+shout += "!!!!"
+alert(shout);
+
+//build a story maker- challenge
+var story = prompt("Please type an adjective");
+story += "this is a lazy programmer";
+var story = prompt("please type a verb");
+story += "he is a fine boy"
+var story = prompt("please type a noun");
+document.write(story);
---
+++ JavaScript-basics/story.js
@@ -0,0 +1,11 @@
+"<h2> There once was a[adjective] programmer who wanted to use JavaScript to[verb] a [noun] </h2>"
+
+var adjective = prompt("please type an adjective");
+var verb = prompt("Please type a verb");
+var noun = prompt("please type a noun");
+var sentence = "<h2> There once was a " + adjective;
+alert("All done. Ready for the message?");
+sentence += " programmer who wanted to use JavaScript to " + verb;
+sentence += " a " + noun + "</h2>";
+document.write(verb);
+document.writeln(sentence);
