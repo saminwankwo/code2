78c00166beccc5819a435331bd7cf2b86ec60cb2
Nwankwo Samuel <nwankwosami@gmail.com>
2019-07-28 23:26:14 +0100

    built a randomNumber guessing gaming@

---
+++ JavaScript-basics/quiz.js
@@ -0,0 +1,32 @@
+// first look at conditionals
+var answer = prompt("what programing language is the name of a gem?");
+if (answer.toUpperCase() === 'RUBY') {
+    document.write("<p> That's Right </p>");
+} else {
+    document.write("<p> sorry, wrong answer</p>");
+}
+
+
+//generate a random number and ask the user to quess it
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var guess = prompt("I am thinking of a number between 1 and 6. what is it?");
+if (parseInt(guess) === randomNumber) {
+    document.write("you guessed the right number");
+} else {
+    document.write("sorry, the Number was " + randomNumber + '</p>');
+}
+
+
+// adding booleen value to our game
+var correctGuess = false; 
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var guess = prompt("I am thinking of a number between 1 and 6. what is it?");
+if (parseInt(guess) === randomNumber) {
+    correctGuess = true;
+}
+ 
+if ( correctGuess ) {
+    document.write("you guessed the right number")
+} else {
+    document.write("sorry, the Number was " + randomNumber + '</p>');
+}
