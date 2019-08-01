20221608efe19ec7494061be1123d8c31d5d0977
Nwankwo Samuel <nwankwosami@gmail.com>
2019-08-01 23:50:40 +0100

    creating a dice app

---
+++ JavaScript-basics/dice.js
@@ -0,0 +1,9 @@
+// generating random numbers using parameters
+
+function alertRandom(upper) {
+    var randomNumber = Math.floor(Math.random() * upper) + 1;
+    alert(randomNumber);
+}
+
+alertRandom(9);
+
