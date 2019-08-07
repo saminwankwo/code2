c4baa32af37b1835aae918394d9e080eb5aac9b6
Nwankwo Samuel <nwankwosami@gmail.com>
2019-08-08 00:01:25 +0100

    creating a list of music playlist

--- Javascript-loops,-arrays-and-objects/shopping.js
+++ Javascript-loops,-arrays-and-objects/shopping.js
@@ -12,6 +12,7 @@ console.log(shoppingList[3]);
 var home = [1, 2, 6, 8, 8, 3, 4, 2, 9, 'jhjgh'];
 home[home.length] = 9;
 home[home.length] = 'holes';
-home.push('phone ', 'home');
+home.push('phone ', 'home'); // adding item to the end of an array
+home.unshift('6', 'ghkk', 'ggfgf'); //adding items to the beginning of an array;
 document.writeln(home);
 // document.write(home.length);
