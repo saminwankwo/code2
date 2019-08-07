dc15e87a5069145a174b12d2d03c09041a6ea550
Nwankwo Samuel <nwankwosami@gmail.com>
2019-08-07 23:36:44 +0100

    first class on array

---
+++ Javascript-loops,-arrays-and-objects/shopping.js
@@ -0,0 +1,17 @@
+// file for learning basic operations in Array data structure
+
+var shoppingList = ['carrot', 'milk', 'eggs'];
+
+// acessing an array
+console.log(shoppingList[0]);
+console.log(shoppingList[3]);
+
+
+
+//number of elements in an array
+var home = [1, 2, 6, 8, 8, 3, 4, 2, 9, 'jhjgh'];
+home[home.length] = 9;
+home[home.length] = 'holes';
+home.push('phone ', 'home');
+document.writeln(home);
+// document.write(home.length);
