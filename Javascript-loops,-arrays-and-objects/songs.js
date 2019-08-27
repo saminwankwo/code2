5cbe7cbda1dc88896739b8938476cf9d64d29c37
Nwankwo Samuel <nwankwosami@gmail.com>
2019-08-27 12:15:01 +0100

    created a song playist using 2-dimensional array

---
+++ Javascript-loops,-arrays-and-objects/songs.js
@@ -0,0 +1,20 @@
+var playList = [
+    ['chandelier', 'Sia'],
+    ['From now on', 'Greatest showman'],
+    
+];
+
+function print(message) {
+    document.write(message);
+}
+
+function printSongs( songs ) {
+    var listHtml = '<ol>';
+    for (var i = 0; i < songs.length; i += 1) {
+        listHtml += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>';
+    }
+    listHtml += '</ol>';
+    print(listHtml);
+}
+
+printSongs(playList);
